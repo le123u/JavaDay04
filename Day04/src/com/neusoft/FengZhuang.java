a55806fefdfc1fb7aed1afa74339d5648fc3c752
le123u <63946085+le123u@users.noreply.github.com>
2020-04-26 21:01:11 +0800

    JavaDay04基础练习

---
+++ Day04/src/com/neusoft/FengZhuang.java
@@ -0,0 +1,15 @@
+package com.neusoft;
+
+/**
+ * @author Liu
+ * @date 2020/4/26 20:36
+ */
+public class FengZhuang {
+    /**
+     * 封装
+     * 封装实际上是将属性封装起来  属性变为私有的，无法访问 要提供公有的方法 对其进行访问
+     * private 代表最小权限
+     * 可以修饰成员变量 和成员方法
+     * 被private修饰的成员方法和成员变量，只有在本类才能访问
+     */
+}
